a7a01c5ed23be234f9072d671ca07e895953f361
unicorngirl2010 <86110608+unicorngirl2010@users.noreply.github.com>
2022-03-11 17:03:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var x = document.getElementById("myAudio");
 function new_image() {
 
     fabric.Image.fromURL('BirthdayImage.jpg', function (Img) {
-
+        block_image_object=Img;
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
         block_image_object.set({
